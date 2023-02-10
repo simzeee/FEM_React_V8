b06e337941e4d3b94e9e21889b59c5dafda700a2
John Sims <jwsims816@gmail.com>
2023-02-10 13:18:34 -0500

    useState update esLint

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import Pet from './Pet'
+import {createRoot} from 'react-dom/client'
+import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese"/>
-      <Pet name="Pepper" animal="bird" breed="Cockatiel"/>
-      <Pet name="Doink" animal="cat" breed="Mixed"/>
+     <SearchParams/>
     </div>
   )
 };
 
 const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(<App/>);
