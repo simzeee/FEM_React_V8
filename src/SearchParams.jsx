bcce698f6862b9f656e768431b2c8c4950ed31bd
John Sims <jwsims816@gmail.com>
2023-02-10 13:34:11 -0500

    useState for animal, breed, location

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -2,6 +2,12 @@ import { useState } from 'react';
 
 const SearchParams = () => {
   const [location, setLocation] = useState('');
+  const [animal, setAnimal] = useState('');
+  const [breed, setBreed] = useState('');
+
+  const BREEDS = [];
+  const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile'];
+
   return (
     <div className="search-params">
       <form action="">
@@ -13,6 +19,39 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
           />
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            name=""
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed('');
+            }}
+          >
+            <option>Please choose an option</option>
+            {ANIMALS.map((animal) => {
+              return <option key={animal}>{animal}</option>;
+            })}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            disabled={BREEDS.length === 0}
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+          >
+            <option>Please choose an option</option>
+            {BREEDS.map((breed) => {
+              return <option key={breed}>{breed}</option>;
+            })}
+          </select>
         </label>
         <button>Submit</button>
       </form>
