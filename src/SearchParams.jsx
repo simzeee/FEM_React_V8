bf0ded3e7b7aa702d29e2ab64898d95cda270010
John Sims <jwsims816@gmail.com>
2023-02-10 14:25:16 -0500

    fix: refactor SearchParams use Results component

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
-import Pet from './Pet';
 import useBreedList from './useBreedList';
+import Results from './Results';
 
 const SearchParams = () => {
   const [location, setLocation] = useState('');
@@ -76,11 +76,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {
-        pets.map(pet => (
-          <Pet name={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id}/>
-        ))
-      }
+      <Results pets={pets}/>
     </div>
   );
 };
